ce2cf7d04a24946447efd6fc66d13566b19be8dd
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2016-11-14 18:19:20 +0300

    initial mapping improvements; pubchem-bioassays

--- index-pubchem-bioassays.py
+++ index-pubchem-bioassays.py
@@ -17,11 +17,11 @@ def read_and_index_pubchem_bioassays(infile, es, indexfunc):
     t1 = time.time()
     if os.path.isdir(infile):
         for child in os.listdir(infile):
+            c = os.path.join(infile, child)
             if child.endswith(".zip"):
-                c = os.path.join(infile, child)
                 read_and_index_pubchem_bioassays_zipfile(c, es, indexfunc)
             else:
-                read_and_index_pubchem_bioassays_file(child, es, indexfunc)
+                read_and_index_pubchem_bioassays_file(c, es, indexfunc)
             i += 1
     else:
         if infile.endswith(".zip"):
@@ -59,21 +59,36 @@ def read_and_index_pubchem_bioassays_file(infile, es, indexfunc):
         f = gzip.open(infile, 'rt')
     else:
         f = open(infile, 'r')
-    aid = infile[infile.find('/') + 1:infile.find(".json")]
+    aid = infile[infile.rfind('/') + 1:infile.find(".json")]
     r = indexfunc(es, f, 0, aid)
     return r
 
 
+def update_date(date):
+    d = "{}-{}-{}".format(date["std"]["year"], date["std"]["month"],
+                          date["std"]["day"])
+    del (date["std"])
+    return d
+
+
+def update_dates(doc):
+    for data in doc["PC_AssaySubmit"]["data"]:
+        date = data["date"]
+        d = update_date(date)
+        data["date"] = d
+    db = doc["PC_AssaySubmit"]["assay"]["descr"]["aid_source"]["db"]
+    if "date" in db:
+        d = update_date(db["date"])
+        doc["PC_AssaySubmit"]["assay"]["descr"]["aid_source"]["db"]["date"] = d
+    return
+
+
 def es_index_bioassay(es, f, r, aid_):
-    ba = json.load(f)
+    doc = json.load(f)
     # TODO: support for large entries, >800mb; ex:0540001_0541000/540253.json.gz
     if f.tell() < 800400400:
-        for data in ba["PC_AssaySubmit"]["data"]:
-            date = data["date"]["std"]
-            d = "{}-{}-{}".format(date["year"], date["month"], date["day"])
-            del(data["date"]["std"])
-            data["date"] = d
-        aid = ba['PC_AssaySubmit']['assay']['descr']['aid']['id']
+        update_dates(doc)
+        aid = doc['PC_AssaySubmit']['assay']['descr']['aid']['id']
         if str(aid) != aid_:
             print("filename and Assay ids not same, please check '%s' vs '%s'"
                   % (aid, aid_))
@@ -86,7 +101,7 @@ def es_index_bioassay(es, f, r, aid_):
                 r = 0
             print(".", end='', flush=True)
             es.index(index=args.index, doc_type='bioassay',
-                     id=aid, body=json.dumps(ba))
+                     id=aid, body=json.dumps(doc))
             r += f.tell()
         except Exception as e:
             print(e)
@@ -96,7 +111,7 @@ def es_index_bioassay(es, f, r, aid_):
 
 
 def main(es, infile, index):
-    # es.indices.delete(index=index, params={"timeout": "10s"})
+    es.indices.delete(index=index, params={"timeout": "10s"})
     iconfig = json.load(open("pubchem-bioassays-index-config.json", "rt"))
     es.indices.create(index=index, params={"timeout": "20s"},
                       ignore=400, body=iconfig, wait_for_active_shards=1)
@@ -108,9 +123,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Index PubChem Bioassays, using Elasticsearch')
     parser.add_argument('--infile',
-                        # default="1158822.json.gz",
-                        default="/reference/NCBI/pubchem/Bioassay/JSON/",
-                        # default="/reference/NCBI/pubchem/Bioassay/JSON/0000001_0001000.zip",
+                        # default="./data/pubchem-samplefiles/bioassays",
+                        # default="/reference/NCBI/pubchem/Bioassay/JSON/",
+                        default="/reference/NCBI/pubchem/Bioassay/JSON/1158001_1159000.zip",
                         help='input file to index')
     parser.add_argument('--index',
                         default="pubchem-bioassays-test15",
