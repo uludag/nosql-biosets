992b7cbfb983e698c63ff168af0fd004c9a5c733
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2016-11-20 19:02:28 +0300

    sample aggregation queries, PubChem bioassays

--- query-pubchem-bioassays.py
+++ query-pubchem-bioassays.py
@@ -2,92 +2,148 @@
 
 # Sample queries for PubChem Bioassays indices
 
-# TODO: Look at MultiQC for presentation of results
-
 import argparse
 
 from elasticsearch import Elasticsearch
 
-debug = False
-
 
 def query(es, index, qc):
     print("querying %s" % str(qc))
-    asets = es.search(index=index,
-                      body={"size": 0, "query": qc})
-    print("query returned %d entries" % asets['hits']['total'])
+    r = es.search(index=index,
+                  body={"size": 0, "query": qc})
+    nhits = r['hits']['total']
     if debug:
-        for doc in asets['hits']['hits']:
-            aid = doc["_source"]['PC_AssaySubmit']['assay']['descr']['aid']['id']
+        print("query returned %d entries" % nhits)
+        for doc in r['hits']['hits']:
+            aid = doc["_source"]['assay']['descr']['aid']['id']
             print("%s -- " % aid)
+    return nhits
 
 
 def aggquery(es, index, qc, aggqc):
-    print("querying %s" % str(qc))
-    asets = es.search(index=index,
-                      body={"size": 0, "query": qc, "aggs": aggqc})
-    print("query returned %d entries" % asets['hits']['total'])
-    print("dbname bucket has %d entries" %
-          len(asets['aggregations']['dbname']['buckets']))
+    print("querying %s with aggregations" % str(qc))
+    r = es.search(index=index,
+                  body={"size": 0, "query": qc, "aggs": aggqc})
     if debug:
-        for doc in asets['hits']['hits']:
-            aid = doc["_source"]['PC_AssaySubmit']['assay']['descr']['aid']['id']
+        print("agg query returned %d entries" % r['hits']['total'])
+        for doc in r['hits']['hits']:
+            aid = doc["_source"]['assay']['descr']['aid']['id']
             print("%s -- " % aid)
+    return r
 
 
-def query_assayids(es, index):
-    l = [123, 234, 502]
-    for aid in l:
-        qc = {"match": {"PC_AssaySubmit.assay.descr.aid.id": aid}}
-        query(es, index, qc)
+class Tests:
+    def __init__(self, es, index, doc_type):
+        self.es = es
+        self.index = index
+        self.doc_type = doc_type
 
+    # Check number of tested substances for sample assays
+    def check_numberoftestedsubstances(self):
+        tsubts = ((638250, 5), (1120060, 27))
+        for (aid, n) in tsubts:
+            qc = { "match": {"assay.descr.aid.id": aid}}
+            aggqc = {
+                    "tested substances": { "terms": {
+                        "field": "data.sid",
+                        "size": 1
+                }}}
+            r = aggquery(self.es, self.index, qc, aggqc)
+            ts = r['aggregations']['tested substances']
+            a = ts['sum_other_doc_count'] + len(ts['buckets'])
+            if n != a:
+                print("assay has %d tested substances, expected: %d" % (a, n))
 
-def queries(es, index):
-    query_assayids(es, index)
-    qc = {"match": {"PC_AssaySubmit.data.outcome": "active"}}
-    query(es, index, qc)
-    qc = {"query_string": {"query": "cancer"}}
-    query(es, index, qc)
+    def query_sample_assayids(self):
+        l = [123, 1120060, 742589]
+        for aid in l:
+            qc = {"match": {"assay.descr.aid.id": aid}}
+            n = query(self.es, self.index, qc)
+            if n != 1:
+                print("assay with id %d not found" % aid)
 
-
-def aggqueries(es, index):
-    qc = { "match": {"PC_AssaySubmit.data.outcome": "active"}}
-    aggqc = {
-        "dbname": {
-            "terms": {
-                "field": "PC_AssaySubmit.assay.descr.aid_source.db.name",
-                "size": 100
-            },
-            "aggs": {
-                "outcome": {
-                    "terms": {
-                        "field":
-                            "PC_AssaySubmit.assay.descr.activity_outcome_method",
-                        "size": 10
-                    }
-                }
-            }
-        }}
-    aggquery(es, index, qc, aggqc)
+    def sample_aggregation_queries(self):
+        # distribution of outcome methods with databases
+        # for assays with at least one active outcome
+        qc = { "match": {"data.outcome": "active"}}
+        aggqc = {
+            "database": {
+                "terms": {
+                    "field": "assay.descr.aid_source.db.name",
+                    "size": 10},
+                "aggs": {
+                    "outcome_method": {
+                        "terms": {
+                            "field":
+                                "assay.descr.activity_outcome_method",
+                            "size": 10
+                        }}}}}
+        r = aggquery(self.es, self.index, qc, aggqc)
+        if r['hits']['total'] < 4000:
+            print("less than expected number of assays")
+        if r['aggregations']['database']['buckets'][0]['doc_count'] < 3000:
+            print("less than expected number of results")
+        # distribution of substances with databases
+        # for assays with target molecule type protein
+        qc = {"match": {"assay.descr.target.molecule_type": "protein"}}
+        aggqc = {
+            "database": {
+                "terms": {
+                    "field": "assay.descr.aid_source.db.name",
+                    "size": 100},
+                "aggs": {
+                    "substance": {
+                        "terms": {
+                            "field":
+                                "data.sid",
+                            "size": 10
+                        }}}}}
+        r = aggquery(self.es, self.index, qc, aggqc)
+        if r['aggregations']['database']['buckets'][0]\
+            ['substance']['buckets'][0]['doc_count'] < 200:
+            print("less than expected number of substances")
+        # distribution of reference genes with substances
+        # for assays with reference genes defined
+        qc = {"exists": {"field": "assay.descr.xref.xref.gene"}}
+        aggqc = {
+            "substance": {
+                "terms": {
+                    "field": "data.sid",
+                    "size": 100},
+                "aggs": {
+                    "gene": {
+                        "terms": {
+                            "field":
+                                "assay.descr.xref.xref.gene",
+                            "size": 10
+                        }}}}}
+        r = aggquery(self.es, self.index, qc, aggqc)
+        if r['aggregations']['substance']['buckets'][0]['doc_count'] < 20:
+            print("less than expected number of substances")
 
 
 def main(es, index):
-    queries(es, index)
-    aggqueries(es, index)
+    tests = Tests(es, index, "bioassay")
+    tests.query_sample_assayids()
+    tests.sample_aggregation_queries()
+    tests.check_numberoftestedsubstances()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Query PubChem Bioassays Elasticsearch index')
     parser.add_argument('--index',
-                        default="pubchem-bioassays-test14",
+                        default="pubchem-bioassays-test21",
                         help='name of the elasticsearch index')
     parser.add_argument('--host', default="esnode-ruqayyah",
                         help='Elasticsearch server hostname')
     parser.add_argument('--port', default="9200",
                         help="Elasticsearch server port")
+    parser.add_argument('--debug', default=False,
+                        help="print more information")
     args = parser.parse_args()
     host = args.host
     port = args.port
+    debug = args.debug
     con = Elasticsearch(host=host, port=port, timeout=600)
     main(con, args.index)
--- readme.md
+++ readme.md
@@ -3,8 +3,7 @@
 Inspired by the [nosql-tests](https://github.com/weinberger/nosql-tests/)
 project we want to develop scripts for NoSQL indexing and querying of
 sample bioinformatics datasets.
-We are in early stages and for now we only have two scripts which are
-for indexing
+We are in early stages of the project and we have scripts for two datasets only
 
 * [PubChem BioAssay json files](ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/JSON/)
 * [WikiPathways gpml files](http://www.wikipathways.org/index.php/Download_Pathways)
@@ -18,29 +17,28 @@ without extracting them to temporary files, and indexes them.
 TODO:
 
 * Support for entries larger than 800mb
-* Representation of date fields in native date formats, this would
-  improve handling of large entries, as some large entries have
-  more than 300k date values
+* Use bulk queries, vary number of entries in the bulks depending on size 
  
 ### Notes on WikiPathways dataset 
 
 [index-wikipathways.py](index-wikipathways.py) script reads
-the zipped WikiPathhways gpml files,
+the zipped WikiPathways gpml files,
 without extracting them to temporary files, and indexes them.
 
 ### Elasticsearch server settings
 Since some of the PubChem BioAssay json files are large we need to change
 two Elasticsearch default settings to higher values:
-todo: ES_JAVA_OPTS
 
-* Set `ES_MIN_MEM` AND `ES_MAX_MEM` environment variables to at least 1 and 14 GBs,
-  defaults are 256mb and 1GB, before calling your Elasticsearch server
-  start script `bin\elasticsearch`
-```Shell
-  export ES_MIN_MEM=1g
-  export ES_MAX_MEM=14g
-```  
-* Set `http.max_content_length: 800mb`, default 100mb,
+* Heap memory
+
+    * _Elasticsearch-5_: Set `-Xms` AND `-Xmx` JVM settings to at least 14 GB,
+    in configuration file `config\jvm.options`
+
+    * _Elasticsearch-2_: Set `ES_MIN_MEM` AND `ES_MAX_MEM` environment variables to at least 1 and 14 GBs,
+    (defaults are 256mb and 1GB), before calling your Elasticsearch server
+    start script `bin\elasticsearch`
+
+- Set `http.max_content_length: 800mb`, default 100mb,
   in your Elasticsearch configuration file `config/elasticsearch.yml`
 * Large entries mean much garbage collection activity;
   [make sure garbage collection is fast](https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-configuration-memory.html) 
