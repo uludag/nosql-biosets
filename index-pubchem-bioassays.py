8ac3f010e7609fbfe0d8f11e85217066ccc127d1
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2016-11-20 15:18:45 +0300

    PC_BioassaySubmit layer in json files is now used as doc type name

--- index-pubchem-bioassays.py
+++ index-pubchem-bioassays.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''
+Index PubChem Bioassay json files with Elasticsearch
 
+TODO: support for large entries, >200mb; ex:540253.json.gz
+'''
 import argparse
 import gzip
 import json
@@ -8,9 +12,17 @@ import time
 from zipfile import ZipFile
 from elasticsearch import Elasticsearch
 
+# Document type name for the Elascticsearch index entries
+doctype = 'PC_BioassaySubmit'
+# Maximum size of uncompressed files that should be indexed
+MaxEntrySize = 200*1024*1024
+# Maximum total size of uncompressed files indexed
+# before an Elasticsearch  _refresh call (~equivalent of database commits)
+MaxBulkSize = 256*1024
 
-# Read PubChem Bioassays json files, index using the index function specified
-# Iterate over files in the folder if the input file is a folder
+
+# Read given bioassay json file, index using the index function specified
+# If the input file is a folder then iterate over files in the folder
 def read_and_index_pubchem_bioassays(infile, es, indexfunc):
     print("Reading %s " % infile)
     i = 0
@@ -35,7 +47,7 @@ def read_and_index_pubchem_bioassays(infile, es, indexfunc):
     return None
 
 
-# Read PubChem Bioassays zip file, index using the index function specified
+# Read given bioassays zip file, index using the index function specified
 def read_and_index_pubchem_bioassays_zipfile(zipfile, es, indexfunc):
     print("\nProcessing %s " % zipfile)
     i = 0
@@ -44,7 +56,7 @@ def read_and_index_pubchem_bioassays_zipfile(zipfile, es, indexfunc):
         for fname in myzip.namelist():
             aid = fname[fname.find('/')+1:fname.find(".json")]
             # check whether the entry is already indexed
-            if not es.exists(index=args.index, doc_type='bioassay', id=aid):
+            if not es.exists(index=args.index, doc_type=doctype, id=aid):
                 with myzip.open(fname) as jfile:
                     f = gzip.open(jfile, 'rt')
                     r = indexfunc(es, f, r, aid)
@@ -53,7 +65,7 @@ def read_and_index_pubchem_bioassays_zipfile(zipfile, es, indexfunc):
     return i
 
 
-# Read PubChem Bioassays file, index using the index function specified
+# Read given bioassay file, index using the index function specified
 def read_and_index_pubchem_bioassays_file(infile, es, indexfunc):
     if infile.endswith(".gz"):
         f = gzip.open(infile, 'rt')
@@ -64,9 +76,10 @@ def read_and_index_pubchem_bioassays_file(infile, es, indexfunc):
     return r
 
 
+# Return given date in format YY-MM-DD
 def update_date(date):
-    d = "{}-{}-{}".format(date["std"]["year"], date["std"]["month"],
-                          date["std"]["day"])
+    d = "{}-{:02}-{:02}".format(date["std"]["year"], date["std"]["month"],
+                                date["std"]["day"])
     del (date["std"])
     return d
 
@@ -85,8 +98,7 @@ def update_dates(doc):
 
 def es_index_bioassay(es, f, r, aid_):
     doc = json.load(f)
-    # TODO: support for large entries, >800mb; ex:0540001_0541000/540253.json.gz
-    if f.tell() < 800400400:
+    if f.tell() < MaxEntrySize:
         update_dates(doc)
         aid = doc['PC_AssaySubmit']['assay']['descr']['aid']['id']
         if str(aid) != aid_:
@@ -94,24 +106,27 @@ def es_index_bioassay(es, f, r, aid_):
                   % (aid, aid_))
             return r
         try:
-            if r>0 and (r+f.tell() > 6666*12):  # refresh as needed
+            if r>0 and (r+f.tell() > MaxBulkSize):
+                # refresh to avoid memory errors
                 print("r", end='', flush=True)
                 es.indices.refresh(index=args.index)
                 es.indices.clear_cache(index=args.index)
                 r = 0
             print(".", end='', flush=True)
-            es.index(index=args.index, doc_type='bioassay',
-                     id=aid, body=json.dumps(doc))
+            docx = {"assay":doc['PC_AssaySubmit']['assay'],
+                    "data":doc['PC_AssaySubmit']['data']}
+            es.index(index=args.index, doc_type=doctype,
+                     id=aid, body=json.dumps(docx))
             r += f.tell()
         except Exception as e:
             print(e)
     else:
-        print("large entry %s %d" % (aid_, f.tell()))
+        print("\nlarge entry %s %d\n" % (aid_, f.tell()))
     return r
 
 
 def main(es, infile, index):
-    es.indices.delete(index=index, params={"timeout": "10s"})
+    #es.indices.delete(index=index, params={"timeout": "10s"})
     iconfig = json.load(open("pubchem-bioassays-index-config.json", "rt"))
     es.indices.create(index=index, params={"timeout": "20s"},
                       ignore=400, body=iconfig, wait_for_active_shards=1)
@@ -128,7 +143,7 @@ if __name__ == '__main__':
                         default="/reference/NCBI/pubchem/Bioassay/JSON/1158001_1159000.zip",
                         help='input file to index')
     parser.add_argument('--index',
-                        default="pubchem-bioassays-test15",
+                        default="pubchem-bioassays-test1",
                         help='name of the elasticsearch index')
     parser.add_argument('--host', default="esnode-khadija",
                         help='Elasticsearch server hostname')
