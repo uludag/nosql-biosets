ea61f94276137cecc654f18280f49baffb25abc1
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2017-05-21 09:32:40 +0300

    more query-all test query terms

--- tests/queryall.py
+++ tests/queryall.py
@@ -11,10 +11,7 @@ typeaggs = {
     "type": {
         "terms": {
             "field": "_type",
-            "size": 50,
-            "order": {
-                "_count": "desc"
-            }}}}
+            "size": 10}}}
 
 
 class QueryallTests(unittest.TestCase):
@@ -25,28 +22,29 @@ class QueryallTests(unittest.TestCase):
     finally:
         pass
     es = Elasticsearch(host=conf['host'], port=conf['port'], timeout=600)
-    index = ""
 
     def query(self, qterms, doctype=None):
-        qc = {"match": {"_all": ' '.join(qterms)}}
+        qc = {"match": {"_all": ' AND '.join(qterms)}}
         r = self.es.search(doc_type=doctype,
                            body={"size": 0, "query": qc, 'aggs': typeaggs})
         return r
 
-    def test_query(self):
-        """Makes a sample query with sample query term 'p53',
-        grouping results with document types
-        then the number of results are checked by repeating
-        the same query with specifying document type with each type"""
-        qterms = ['p53']
+    def querywithtypes(self, qterms):
         r = self.query(qterms)
         types = r['aggregations']['type']['buckets']
         self.assertGreater(len(types), 1)
         for t in types:
             doctype = t['key']
-            print("doctype: %s" % doctype)
             r = self.query(qterms, doctype=doctype)
             self.assertEqual(r['hits']['total'], t['doc_count'])
+
+    def test_queries(self):
+        """Makes a query with given query terms, grouping the results
+        with document types, number of results are then compared by repeating
+        the same query with specifying document type with each type"""
+        l = [["p53", "kinase"], ["xylose"], ["naringenin"], ["acetyl"]]
+        for qterms in l:
+            self.querywithtypes(qterms)
 
 
 if __name__ == '__main__':
