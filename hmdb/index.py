3f2a4d0fdf4874ac10d814df1ac17ae75d1de62b
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2017-06-19 18:45:26 +0300

    initial text index for HMDB metabolites

--- hmdb/index.py
+++ hmdb/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-"""Index HMDB protein/metabolite records in xml http://www.hmdb.ca/downloads """
+"""Index HMDB protein/metabolite records in xml,
+ download page: http://www.hmdb.ca/downloads """
 from __future__ import print_function
 
 import argparse
@@ -9,6 +10,8 @@ from gzip import GzipFile
 from zipfile import ZipFile
 
 import xmltodict
+from pymongo import IndexModel
+
 from nosqlbiosets.dbutils import DBconnection
 
 
@@ -59,12 +62,20 @@ class Indexer(DBconnection):
         docid = entry['accession']
         spec = {"_id": docid}
         try:
-            # if not self.exists(id):
-            self.mcl.update(spec, json.loads(json.dumps(entry)), upsert=True)
+            self.mcl.update(spec, entry, upsert=True)
             return True
         except Exception as e:
             print(e)
         return False
+
+
+def mongodb_textindex(mdb, doctype):
+    if doctype == 'metabolite':
+        index = IndexModel([
+            ("description", "text"), ("name", "text"),
+            ("taxanomy.description", "text")])
+        mdb.create_indexes([index])
+    return
 
 
 def main(infile, index, doctype, db, host, port):
@@ -74,6 +85,7 @@ def main(infile, index, doctype, db, host, port):
         indxr.es.indices.refresh(index=index)
     else:
         parse_hmdb_xmlfile(infile, indxr.mongodb_index_hmdb_entry)
+        mongodb_textindex(indxr.mcl, doctype)
 
 
 if __name__ == '__main__':
@@ -81,18 +93,18 @@ if __name__ == '__main__':
         description='Index HMDB protein/metabolite entries,'
                     ' with Elasticsearch or MongoDB')
     parser.add_argument('-infile', '--infile',
-                        default="./data/hmdb_proteins-first10.xml.gz",
+                        default="../data/hmdb_proteins-first10.xml.gz",
                         help='Input file name')
     parser.add_argument('--index',
-                        default="hmdb-kbase",
-                        help='Name of the Elasticsearch index')
+                        default="biosets",
+                        help='Name of the Elasticsearch index or MongoDB db')
     parser.add_argument('--doctype',
                         help='Document type (protein or metabolite)')
     parser.add_argument('--host',
-                        help='Elasticsearch server hostname')
+                        help='Elasticsearch or MongoDB server hostname')
     parser.add_argument('--port',
-                        help="Elasticsearch server port number")
-    parser.add_argument('--db', default='Elasticsearch',
+                        help="Elasticsearch or MongoDB server port number")
+    parser.add_argument('--db', default='Elasticsearch_',
                         help="Database: 'Elasticsearch' or 'MongoDB'")
     args = parser.parse_args()
     doctype_ = 'metabolite'
