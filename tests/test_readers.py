8a234d07fdb9657d51406074853159d579a80c41
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2017-10-05 12:14:13 +0300

    Move test data to the tests folder

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -19,20 +19,25 @@ from nosqlbiosets.pubtator.index import parse_pub2gene_lines
 
 class TestDataReaders(unittest.TestCase):
 
-    d = os.path.dirname(os.path.abspath(__file__))
+    data = os.path.dirname(os.path.abspath(__file__)) + "/data/"
+    modelseedcompounds = data + "modelseed/compounds.tsv"
+    modelseedreactions = data + "modelseed/reactions.tsv"
 
+    @unittest.skipUnless(os.path.exists(modelseedcompounds),
+                         "Missing test data file")
     def test_modelseed_creader(self):
-        infile = self.d + "/data/modelseed/compounds.tsv"
-        idlist = [r for r in read_modelseed_datafile(infile,
+        idlist = [r for r in read_modelseed_datafile(modelseedcompounds,
                                                      updatecompoundrecord)]
         self.assertGreaterEqual(len(idlist), 2000)
         r = idlist[0]
         print(r)
         self.assertEqual(r['_id'], 'cpd00001')
 
+    @unittest.skipUnless(os.path.exists(modelseedreactions),
+                         "Missing test data file")
     def test_modelseed_rreader(self):
-        infile = self.d + "/data/modelseed/reactions.tsv"
-        idlist = [r for r in read_modelseed_datafile(infile,
+        infile = self.data + "modelseed/reactions.tsv"
+        idlist = [r for r in read_modelseed_datafile(modelseedreactions,
                                                      updatereactionrecord)]
         self.assertGreaterEqual(len(idlist), 2000)
         r = idlist[0]
@@ -40,7 +45,7 @@ class TestDataReaders(unittest.TestCase):
         self.assertEqual(r['_id'], 'rxn00001')
 
     def test_rnacentral_idmapping_reader(self):
-        infile = self.d + "/../data/rnacentral-6.0-id_mapping-first1000.tsv"
+        infile = self.data + "rnacentral-6.0-id_mapping-first1000.tsv"
         idlist = [r for r in mappingreader(infile)]
         self.assertEqual(len(idlist), 342)
         r = idlist[0]
@@ -48,19 +53,19 @@ class TestDataReaders(unittest.TestCase):
         self.assertEqual(len(r['mappings']), 11)
 
     def test_ensembl_regbuild_regions_reader(self):
-        infile = self.d + "/../data/hg38.ensrb_features.r88.first100.gff"
+        infile = self.data + "hg38.ensrb_features.r88.first100.gff"
         db = connectgffdb(infile)
         regions = [r for r in regregions(db)]
         self.assertEqual(len(regions), 100)
 
     def test_ensembl_regbuild_motifs_reader(self):
-        infile = self.d + "/../data/hg38.ensrb_motiffeatures.r88.first1000.gff"
+        infile = self.data + "hg38.ensrb_motiffeatures.r88.first1000.gff"
         db = connectgffdb(infile)
         tflist = [r for r in tfs(db)]
         self.assertEqual(len(tflist), 1000)
 
     def test_gene2pubtator_reader(self):
-        infile = self.d + "/../data/gene2pubtator.sample"
+        infile = self.data + "gene2pubtator.sample"
         r = 0
         with open(infile) as inf:
             db = parse_pub2gene_lines(inf, r, 'gene2pub')
@@ -72,8 +77,8 @@ class TestDataReaders(unittest.TestCase):
         self.nhmdbentries += 1
         return True
 
-    hmdbcsfmetabolites = d + "/../data/hmdb_csf_metabolites-first3.xml.gz"
-    hmdbproteins = d + "/../data/hmdb_proteins-first10.xml.gz"
+    hmdbcsfmetabolites = data + "hmdb_csf_metabolites-first3.xml.gz"
+    hmdbproteins = data + "hmdb_proteins-first10.xml.gz"
 
     @unittest.skipUnless(os.path.exists(hmdbproteins) and
                          os.path.exists(hmdbcsfmetabolites),
@@ -86,8 +91,8 @@ class TestDataReaders(unittest.TestCase):
         parse_hmdb_xmlfile(self.hmdbproteins, self.hmdb_reader_helper)
         self.assertEqual(self.nhmdbentries, 10)
 
-    compoundsxreffile = d + "/../metanetx/data/chem_xref-head.tsv"
-    compoundsfile = d + "/../metanetx/data/chem_prop-head.tsv"
+    compoundsxreffile = data + "metanetx/chem_xref-head.tsv"
+    compoundsfile = data + "metanetx/chem_prop-head.tsv"
 
     @unittest.skipUnless(os.path.exists(compoundsfile) and
                          os.path.exists(compoundsxreffile),
@@ -101,8 +106,8 @@ class TestDataReaders(unittest.TestCase):
                                  'GPRLSGONYQIRFK-UHFFFAOYSA-N')
                 break
 
-    reactsxreffile = d + "/../metanetx/data/reac_xref-head.tsv"
-    reactsfile = d + "/../metanetx/data/reac_prop-head.tsv"
+    reactsxreffile = data + "metanetx/reac_xref-head.tsv"
+    reactsfile = data + "metanetx/reac_prop-head.tsv"
 
     @unittest.skipUnless(os.path.exists(reactsfile) and
                          os.path.exists(reactsxreffile),
@@ -124,7 +129,7 @@ class TestDataReaders(unittest.TestCase):
         else:
             return False
 
-    keggtarfile = d + "/../data/kegg/xml/kgml/metabolic/organisms/hsa.tar.gz"
+    keggtarfile = data + "kegg/xml/kgml/metabolic/organisms/hsa.tar.gz"
 
     @unittest.skipUnless(os.path.exists(keggtarfile),
                          "Missing test files")
@@ -134,7 +139,7 @@ class TestDataReaders(unittest.TestCase):
                                        self.kegg_xmlreader_helper)
         self.assertEqual(self.nkeggentries, 4)
 
-    psammmodelfiles = d + "/data/psamm/sbml/"
+    psammmodelfiles = data + "psamm/sbml/"
 
     @unittest.skipUnless(os.path.exists(psammmodelfiles),
                          "Missing test files folder")
