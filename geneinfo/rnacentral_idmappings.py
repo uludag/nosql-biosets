782c83faa90ff2026fb07153c2565019ee8e475d
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2017-10-11 18:50:30 +0300

    MongoDB support for RNAcentral id-mapping indexer

--- geneinfo/rnacentral_idmappings.py
+++ geneinfo/rnacentral_idmappings.py
@@ -3,13 +3,16 @@
 import argparse
 import csv
 import gzip
-import json
 import logging
 import os
 import time
 
-from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk
+
+from nosqlbiosets.dbutils import DBconnection
+
+DOCTYPE = "rnacentral"
+CHUNKSIZE = 124
 
 
 # Reader for RNAcentral id mappings
@@ -45,7 +48,7 @@ def mappingreader(infile):
 def es_index_idmappings(es, csvfile, reader):
     for ok, result in streaming_bulk(
             es, reader(csvfile),
-            index=args.index, doc_type='rnacentralidmapping', chunk_size=512
+            index=args.index, doc_type=DOCTYPE, chunk_size=CHUNKSIZE
     ):
         if not ok:
             action, result = result.popitem()
@@ -54,35 +57,44 @@ def es_index_idmappings(es, csvfile, reader):
     return
 
 
-def main(es, infile, index):
-    if es.indices.exists(index=index):
-        es.indices.delete(index=index, params={"timeout": "10s"})
-    indxcfg = {"settings": {
-        "index.number_of_replicas": 0, "index.refresh_interval": "360s"}}
-    r = es.indices.create(index=index, params={"timeout": "10s"},
-                          wait_for_active_shards=1, body=indxcfg)
-    logging.debug(r)
-    es_index_idmappings(es, infile, mappingreader)
-    es.indices.refresh(index=index)
+def mongodb_index_idmappings(mdbi, csvfile, reader):
+    entries = list()
+    for entry in reader(csvfile):
+        entries.append(entry)
+        if len(entries) == CHUNKSIZE:
+            mdbi[DOCTYPE].insert_many(entries)
+            entries.clear()
+    mdbi[DOCTYPE].insert_many(entries)
+    return
+
+
+def main(dbc, infile, index):
+    if dbc.db == "Elasticsearch":
+        es_index_idmappings(dbc.es, infile, mappingreader)
+        dbc.es.indices.refresh(index=index)
+    else:  # "MongoDB"
+        mongodb_index_idmappings(dbc.mdbi, infile, mappingreader)
 
 
 if __name__ == '__main__':
-    conf = {"host": "localhost", "port": 9200}
     d = os.path.dirname(os.path.abspath(__file__))
-    try:
-        conf = json.load(open(d + "/../conf/elasticsearch.json", "r"))
-    finally:
-        pass
     parser = argparse.ArgumentParser(
         description='Index RNAcentral id mappings with Elasticsearch')
-    parser.add_argument('--infile', required=True, help='Input file to index')
+    parser.add_argument('--infile',
+                        default=d+"/../tests/data/"
+                                  "rnacentral-v7-id_mapping-first100.tsv",
+                        # required=True,
+                        help='Input file to index')
     parser.add_argument('--index',
-                        default="rnacentral-idmapping",
+                        default="rnacentral",
                         help='Name of the Elasticsearch index')
-    parser.add_argument('--host', default="bio2rdf",
+    parser.add_argument('--host',
                         help='Elasticsearch server hostname')
-    parser.add_argument('--port', default="9200",
+    parser.add_argument('--port',
                         help="Elasticsearch server port")
+    parser.add_argument('--db', default='MongoDB',
+                        help="Database: 'Elasticsearch' or 'MongoDB'")
     args = parser.parse_args()
-    con = Elasticsearch(host=args.host, port=args.port, timeout=3600)
-    main(con, args.infile, args.index)
+    dbc_ = DBconnection(args.db, args.index, host=args.host,
+                        port=args.port)
+    main(dbc_, args.infile, args.index)
