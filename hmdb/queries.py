d0071e8cdc5462fcd4c897c84424ea8b37f39695
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2017-10-30 17:31:40 +0300

    Add more DrugBank test queries

--- hmdb/queries.py
+++ hmdb/queries.py
@@ -7,6 +7,7 @@ from nosqlbiosets.dbutils import DBconnection
 DOCTYPE = 'drugbankdrug'
 
 
+# todo: move tests to a test class
 class QueryDrugBank(unittest.TestCase):
     index = "drugbank"
     db = "MongoDB"
@@ -19,20 +20,38 @@ class QueryDrugBank(unittest.TestCase):
         c = self.mdb[DOCTYPE].find(qc, projection=projection, limit=limit)
         return c
 
-    def distinctquery(self, key, qc=None):
-        r = self.dbc.mdbi[DOCTYPE].distinct(key, filter=qc)
+    def distinctquery(self, key, qc=None, sort=None):
+        r = self.dbc.mdbi[DOCTYPE].distinct(key, filter=qc, sort=sort)
         return r
 
     def test_distinct_classes(self):
         key = "classification.class"
         names = self.distinctquery(key)
         self.assertIn("Carboxylic Acids and Derivatives", names)
+        assert len(names) == 242
+
+    def test_distinct_pfam_classes(self):
+        key = "transporters.transporter.polypeptide.pfams.pfam.name"
+        names = self.distinctquery(key)
+        self.assertIn("Alpha_kinase", names)
+        assert len(names) == 86
+
+    def test_distinct_go_classes(self):
+        key = "transporters.transporter.polypeptide.go-classifiers." \
+              "go-classifier.description"
+        names = self.distinctquery(key)
+        self.assertIn("lipid transport", names)
+        assert len(names) == 1114
 
     def test_distinct_atc_codes(self):
         key = "atc-codes.atc-code.level.#text"
         atc_codes = self.distinctquery(key)
         self.assertIn("Direct thrombin inhibitors", atc_codes)
         assert len(atc_codes) == 940
+        key = "atc-codes.atc-code.code"
+        atc_codes = self.distinctquery(key)
+        self.assertIn("A10AC04", atc_codes)
+        assert len(atc_codes) == 3470
 
     def test_get_atc_codes(self):
         project = {"atc-codes": 1}
@@ -44,6 +63,15 @@ class QueryDrugBank(unittest.TestCase):
         r = list(self.query(qc, projection=project))
         expected = "C10BX04"
         assert r[0]["atc-codes"]["atc-code"][0]["code"] == expected
+
+    def test_get_drug_interactions(self):
+        project = {"drug-interactions": 1}
+        qc = {"_id": "DB00001"}
+        r = list(self.query(qc, projection=project))
+        assert len(r) == 1
+        interactions = r[0]["drug-interactions"]["drug-interaction"]
+        assert interactions[1]["drugbank-id"] == 'DB00346'
+        assert interactions[1]["name"] == 'Alfuzosin'
 
 
 if __name__ == '__main__':
--- hmdb/readme.md
+++ hmdb/readme.md
@@ -2,18 +2,17 @@
 ## Index/query scripts for HMDB and DrugBank xml datasets
 
 * [index.py](index.py) Index HMDB proteins and metabolites datasets.
-
-Tests made with HMDB version 4.0
+  Tests made with HMDB version 4.0
 
 * [../tests/test_hmdb_queries.py](../tests/test_hmdb_queries.py)
- Includes example queries
+  Includes example queries
 
 * [drugbank.py](drugbank.py) Index DrugBank xml dataset with MongoDB,
- or save drug interactions as NetworkX graph file
+  or save drug interactions as NetworkX graph file.
+  Tests made with DrugBank version 5.0
 
-Tests made with DrugBank version 5.0
-
-* [query.py](query.py) Query DrugBank dataset indexed with MongoDB
+* [queries.py](queries.py) Query API for DrugBank data indexed with MongoDB,
+  _at its early stages_
 
 
 ### Usage HMDB
@@ -41,10 +40,16 @@ mkdir -p data
 # Download DrugBank xml dataset, drugbank_all_full_database.xml.zip
 # to the data folder, requires registration
 
-# Index with MongoDB  ~12m
+# Index with MongoDB,  takes ~12m
 ./hmdb/drugbank.py --infile ./data/drugbank_all_full_database.xml.zip --db MongoDB
 
-# Save as NetworkX graph
+# Save as NetworkX graph,  takes ~12m,  #edges = 578072, #nodes = 2550
 ./hmdb/drugbank.py --infile ./data/drugbank_all_full_database.xml.zip --db NetworkX
 
 ```
+
+## Related work
+
+* https://github.com/ashrafsarhan/drugbank-relational-database
+  Project home page includes graph of the DrugBank schema which could be useful
+  in undestanding the DrugBank data
