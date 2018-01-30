03963a1f9b4c0a1cabd71372721020791d995909
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2018-01-30 08:42:56 +0300

    Project setup file, minor review

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import setup, find_packages
 
 requirements = open(os.path.join(os.path.dirname(__file__),
                                  'requirements.txt')).readlines()
+
+packages = find_packages()
+
 setup(
        name='nosqlbiosets',
        version='0.0.2',
        install_requires=requirements,
        packages=find_packages(),
-       data_files=[('data', ['data/gene2pubtator.sample'])],
-       description='Scripts for indexing selected bioinformatics datasets'
-                   'with NoSQL databases',
+       data_files=[('data', [])],
+       description='Index/query scripts '
+                   'for selected free bioinformatics datasets',
        classifiers=[
               'Intended Audience :: Science/Research',
               'Development Status :: 3 - Alpha'],
